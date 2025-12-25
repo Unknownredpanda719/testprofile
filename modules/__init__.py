9e70f8bc9cef292d063592cba7230638ada8e555
Unknownredpanda719 <sahiljhamb95@outlook.com>
2025-12-25 19:57:57 +0000

    Add UK programmes and careers to module exports

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,13 @@
-"""
-Education ROI Engine - Core Modules
-"""
-
 from .psychometric_engine import PsychometricAssessment
 from .recommendation_engine import RecommendationEngine
 from .roi_calculator import ROICalculator
+from .uk_programmes import get_programmes_for_pathway
+from .uk_careers import get_careers_for_field
 
 __all__ = [
     'PsychometricAssessment',
     'RecommendationEngine', 
-    'ROICalculator'
+    'ROICalculator',
+    'get_programmes_for_pathway',
+    'get_careers_for_field'
 ]
